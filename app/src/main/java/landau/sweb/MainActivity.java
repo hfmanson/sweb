b240b57853d0ad0176fb86e50c97eaed22a9d744
Henri Manson <h.manson@aal-audio.com>
2023-03-08 15:55:51 +0100

    Use androidx

--- app/src/main/java/landau/sweb/MainActivity.java
+++ app/src/main/java/landau/sweb/MainActivity.java
@@ -33,8 +33,6 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.Html;
 import android.text.TextWatcher;
@@ -75,6 +73,9 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import org.json.JSONArray;
 import org.json.JSONException;
